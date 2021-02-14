f9e1343db6412e3623d643c31092a71462ff37b5
Fab-13 <52647783+Fab-13@users.noreply.github.com>
2021-02-13 22:05:46 -0700

    Main

--- main.go
+++ main.go
@@ -1,10 +1,29 @@
 package main
 
 import (
-	"log"	
+	"fmt"
+	"log"
 	"net/http"
 )
 
+const tpl = `
+	RequestURI: %v
+	Host:       %v
+	Form:       %v
+	Some:       %v
+`
+
+func reflectForm(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Fprintf(w, fmt.Sprintf(tpl, 
+		r.RequestURI, 
+		r.Host, 
+		r.Form,
+		r.Form.Get("Some"),
+	))
+}
+
 func main() {
+	http.HandleFunc("/", reflectForm)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
